d5f77d70d3fc2aa525ecc56a138650da2f8ed309
Eun-Ng <dhkdldms@gmail.com>
2022-05-25 22:45:32 +0900

    Add Inheritance

--- JavaScript/ES6/es6prac.js
+++ JavaScript/ES6/es6prac.js
@@ -175,11 +175,11 @@ console.log(shirts.pGender);
 console.log(Array.prototype);
 console.log(shirts.__proto__); // 부모 prototype 탐색
 
-const 부모 = {성: "남궁"};
-const 자식 = {};
+// const 부모 = {성: "남궁", 나이: 50};
+// const 자식 = {};
 
-자식.__proto__ = 부모;
-console.log(자식.성);
+// 자식.__proto__ = 부모;
+// console.log(자식.성);
 
 let numArr = [1, 2, 3, 4];
 
@@ -193,3 +193,133 @@ Array.prototype.removeNumber = function (e) {
 
 numArr.removeNumber(4);
 console.log(numArr); // [1, 2, 3]
+
+// ES5 Inheritance
+// Object.create(프로토타입 Object);
+
+// const 부모 = {성: "남궁", 나이: 60};
+// const 자식 = Object.create(부모);
+// const 손주 = Object.create(자식);
+// 자식.나이 = 33;
+// 손주.나이 = 1;
+// console.log(자식.나이);
+// console.log(손주.나이);
+
+// ES6 Inheritance
+
+class 부모 {
+  constructor(param) {
+    this.성 = param;
+    // this.helloWorld = function () {
+    //   console.log("Hello World!");
+    // }; // 자식에 상속 가능
+  }
+  helloWorld() {
+    console.log("Hello World!"); // 부모 prototype에만 추가됨
+  }
+}
+
+const 자식 = new 부모("남궁");
+console.log(자식);
+console.log(자식.__proto__);
+console.log(Object.getPrototypeOf(자식));
+
+// extends & super
+class 할아버지 {
+  constructor(name) {
+    this.성 = "남궁";
+    this.name = name;
+  }
+}
+
+class 아버지 extends 할아버지 {
+  constructor(name) {
+    super(name);
+    this.나이 = 50;
+  }
+}
+
+const 아버지1 = new 아버지("아무개");
+console.log(아버지1);
+
+// getter & setter
+class somebody {
+  constructor(나이) {
+    this.name = "김아무개";
+    this.age = Number(나이);
+  }
+  get koreanAge() {
+    return this.age + 1;
+  }
+  set setAge(나이) {
+    this.age = Number(나이);
+  }
+}
+
+const somebody1 = new somebody();
+somebody1.setAge = "26";
+console.log(somebody1.koreanAge);
+
+// practice
+class Dog {
+  constructor(종, 털색깔) {
+    this.종 = String(종);
+    this.털색깔 = String(털색깔);
+  }
+  한살먹기() {
+    if (this instanceof Cat) {
+      return this.나이 + 1;
+    }
+  }
+}
+
+const 말티즈 = new Dog("말티즈", "하얀색");
+const 시바 = new Dog("시바", "갈색");
+console.log(말티즈);
+
+class Cat extends Dog {
+  constructor(종, 털색깔, 나이) {
+    super(종, 털색깔);
+    this.나이 = Number(나이);
+  }
+}
+
+const 스코티쉬폴드 = new Cat("스코티쉬폴드", "회색", 3);
+const 코숏 = new Cat("코숏", "노란색", 2);
+console.log(스코티쉬폴드);
+console.log(코숏.한살먹기());
+
+class Unit {
+  constructor(공격력, 체력) {
+    this.공격력 = 5;
+    this.체력 = 100;
+  }
+  get battlePoint() {
+    return Number(this.공격력 + this.체력);
+  }
+  set heal(힐) {
+    this.체력 = Number(this.체력 + 힐);
+  }
+}
+
+const Unit1 = new Unit();
+console.log(Unit1.battlePoint);
+Unit1.heal = 50;
+console.log(Unit1.체력);
+
+let data = {
+  odd: [],
+  even: [],
+  set setter(num) {
+    // if ((num/2) = 1) {
+    //   odd = [...num]
+    // } else {
+    //   even = [...num]
+    // }
+  },
+  get getter() {
+    return this.num.sort();
+  },
+};
+
+// data.setter(1, 2, 3, 4, 5);
