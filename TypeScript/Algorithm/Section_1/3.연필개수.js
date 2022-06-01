aaf9f6628a9548c01673001b480142e2aa922c4e
Eun-Ng <dhkdldms@gmail.com>
2022-06-02 03:24:41 +0900

    Add Section 1-3

---
+++ TypeScript/Algorithm/Section_1/3.연필개수.js
@@ -0,0 +1,13 @@
+const pencil = (n) => {
+    let answer;
+    // answer = Math.ceil(n / 12); // 간단한 방법
+    if (n / 12 === 0) {
+        answer = n / 12;
+    }
+    else if (n / 12 >= 1) {
+        answer = Math.floor(n / 12 + 1);
+    }
+    return answer;
+};
+console.log(pencil(25));
+console.log(pencil(178));
