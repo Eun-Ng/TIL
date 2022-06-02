abd33156dea9372f12c2771b49f52ca630e753ac
Eun-Ng <dhkdldms@gmail.com>
2022-06-03 02:07:47 +0900

    Add Math.min()

--- TypeScript/Algorithm/Section_1/5.최솟값구하기.js
+++ TypeScript/Algorithm/Section_1/5.최솟값구하기.js
@@ -9,3 +9,10 @@ const minimumNum = (arr) => {
 };
 const arr = [5, 3, 7, 11, 2, 16, 17];
 console.log(minimumNum(arr));
+// Math 객체 사용
+const minimumNum2 = (arr2) => {
+    let answer = Math.min(...arr2); // 스프레드 연산자로 배열 분해. 그냥 배열객체로 넘기면 안받아짐.
+    return answer;
+};
+const arr2 = [5, 3, 7, 11, 2, 16, 17];
+console.log(minimumNum2(arr2));
