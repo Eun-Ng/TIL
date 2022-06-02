81c5980fb0b4a14a48131494f2f2bf1a6c51aef2
Eun-Ng <dhkdldms@gmail.com>
2022-06-03 01:57:46 +0900

    Add Section 1-5

---
+++ TypeScript/Algorithm/Section_1/5.최솟값구하기.ts
@@ -0,0 +1,12 @@
+const minimumNum = (arr: number[]): number => {
+  let answer: number = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < answer) {
+      answer = arr[i];
+    }
+  }
+  return answer;
+};
+
+const arr = [5, 3, 7, 11, 2, 16, 17];
+console.log(minimumNum(arr));
