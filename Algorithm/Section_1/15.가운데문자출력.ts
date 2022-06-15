cf54a3ffe80f64e680c1c7fbd34a28b4f0f3c667
Eun-Ng <dhkdldms@gmail.com>
2022-06-15 23:42:46 +0900

    Add Section 1-15

---
+++ Algorithm/Section_1/15.가운데문자출력.ts
@@ -0,0 +1,14 @@
+const middleStr = (a: string): string => {
+  let answer: string = "";
+  let middle: number = Math.floor(a.length / 2);
+
+  if (a.length % 2 === 1) {
+    answer = a.substring(middle, middle + 1);
+  } else {
+    answer = a.substring(middle - 1, middle + 1);
+  }
+  return answer;
+};
+
+console.log(middleStr("study"));
+console.log(middleStr("Boom"));
