5869af0aa17e9d435b77c5075d1367c4736ab962
Eun-Ng <dhkdldms@gmail.com>
2022-06-18 00:10:28 +0900

    Add Section 2-2

---
+++ Algorithm/2_1,2차원_배열_탐색/02_보이는학생.js
@@ -0,0 +1,17 @@
+// 한 줄로 쭉 섰을때 보이는 학생의 수
+// 이중for문은 시간복잡도 비효율
+const student = (a) => {
+    let answer = 0;
+    let sawStudent = [];
+    sawStudent.push(a[0]);
+    for (let i = 1; i < stu.length; i++) {
+        if (sawStudent[sawStudent.length - 1] < a[i]) {
+            sawStudent.push(a[i]);
+        }
+        console.log(sawStudent);
+        answer = sawStudent.length;
+    }
+    return answer;
+};
+const stu = [130, 135, 148, 140, 145, 150, 150, 153];
+console.log(student(stu)); // 5
