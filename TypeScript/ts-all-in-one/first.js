f1d144b9517dc8d7387668bf7f5d2262f0bbef48
Eun-Ng <dhkdldms@gmail.com>
2022-09-02 23:27:26 +0900

    Add TS Studying

---
+++ TypeScript/ts-all-in-one/first.js
@@ -0,0 +1,7 @@
+"use strict";
+// 기본적으로 변수, 속성, 매개변수, 리턴값에 타입이 붙음
+const a = 5;
+const add = (x, y) => {
+    return x + y;
+};
+const obj = { lat: 37.5, lon: 127.5 };
---
+++ TypeScript/ts-all-in-one/first.ts
@@ -0,0 +1,8 @@
+// 기본적으로 변수, 속성, 매개변수, 리턴값에 타입이 붙음
+const a: number = 5;
+
+const add = (x: number, y: number): number => {
+  return x + y;
+};
+
+const obj: {lat: number; lon: number} = {lat: 37.5, lon: 127.5};
