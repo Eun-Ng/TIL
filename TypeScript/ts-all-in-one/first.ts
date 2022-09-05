17659dea271ed15e87009c169dda8b9d845fdec1
Eun-Ng <dhkdldms@gmail.com>
2022-09-05 23:36:56 +0900

    Add TS studying

--- TypeScript/ts-all-in-one/first.ts
+++ TypeScript/ts-all-in-one/first.ts
@@ -1,14 +1,14 @@
 import {NoSubstitutionTemplateLiteral} from 'typescript';
 
 // 기본적으로 변수, 속성, 매개변수, 리턴값에 타입이 붙음
-const a: string = '5';
-const b: number = 5;
-const c: boolean = true;
-const d: undefined = undefined;
-const e: null = null;
+const a = '5';
+const b = 5;
+const c = true;
+const d = undefined;
+const e = null;
 
 // 타입에 고정된 원시값을 넣을수도 있음
-const t: true = true;
+const f: true = true;
 
 // Object
 const obj: {lat: number; lon: number} = {lat: 37.5, lon: 127.5};
@@ -24,9 +24,19 @@ const add2: (x: number, y: number) => number = (x, y) => x + y;
 
 // type Alias
 type Add = (x: number, y: number) => number;
-const add3: Add = (x, y) => x + y;
+const add4: Add = (x, y) => x + y;
 
 // interface
 interface Minus {
   (x: number, y: number): number;
+}
+
+let aa = 123;
+aa = 'hello' as unknown as number;
+
+try {
+  const array: string[] = [];
+  array.push('hello');
+} catch (error) {
+  error;
 }
